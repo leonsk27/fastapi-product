f768304f849f28766502f349b0a7c944e80aa8c1
Henry Taby <henry.taby@gmail.com>
2025-02-18 15:23:03 -0400

    config env

--- app/config.py
+++ app/config.py
@@ -3,9 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    DATABASE_URL_ALEMBIC: str
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
