b53c3405fa177a4d2e7dd159353153d1ffd1c9b2
Henry Taby <henry.taby@gmail.com>
2025-02-18 15:37:30 -0400

    para funcionar en render

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from app.db import create_db_and_tables
@@ -49,3 +51,7 @@ async def read_items():
         </body>
     </html>
     """
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
